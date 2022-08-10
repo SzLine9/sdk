4de4ffe14cc94a0122d39e1c0fa6b41872da00f4
王建民 <original@acodeway.com>
2022-08-10 17:17:16 +0800

    fix

--- src/Sdk/Proxy/Proxy.php
+++ src/Sdk/Proxy/Proxy.php
@@ -10,4 +10,14 @@ class Proxy extends Sdk
     {
         return $this->request('POST', 'shop-api/service/proxy/get-proxy-list', $params, []);
     }
+
+    public function detail(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/proxy/get-proxy-detail', $params, []);
+    }
+
+    public function save(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/proxy/save', $params, []);
+    }
 }
