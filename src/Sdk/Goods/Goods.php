118e7d3d719c9e7173ca67facdbea6dbefdba73d
winterlike <xq13045878572@163.com>
2022-09-13 14:07:12 +0800

    fix

--- src/Sdk/Goods/Goods.php
+++ src/Sdk/Goods/Goods.php
@@ -50,4 +50,17 @@ class Goods extends Sdk
     {
         return $this->request('post', 'shop-api/service/goods/detail', $params, $headers, $options);
     }
+
+    /**
+     * @description 商品的列表
+     * @param array $params
+     * @param array $headers
+     * @param array $options
+     * @return array
+     * @throws SdkException
+     */
+    public function list (array $params, array $headers = [], array $options = []): array
+    {
+        return $this->request('post', 'shop-api/service/goods/list', $params, $headers, $options);
+    }
 }
