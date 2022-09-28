be3f26f43fba2f9a5b5ea8b8599cf4c8dfbe84fc
qqq <dev_line9@163.com>
2022-09-28 17:54:27 +0800

    fix: 增加订单相关的 sdk 接口

---
+++ src/Sdk/Order/Order.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Line9\Sdk\Sdk\Order;
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class Order extends Sdk
+{
+
+    protected function flag(): string
+    {
+        return 'order';
+    }
+
+    /**
+     * @description 订单列表
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function list(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/order/list', $param, []);
+    }
+
+    /**
+     * @description 订单详情
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function detail(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/order/detail', $param, []);
+    }
+
+    /**
+     * @description 订单导出
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function export(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/order/export', $param, []);
+    }
+
+    /**
+     * @description 订单发货
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function expressSend(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/order/express-send', $param, []);
+    }
+
+    /**
+     * @description 订单收货地址
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function orderAddressUpdate(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/order/order-address/update', $param, []);
+    }
+
+    /**
+     * @description 订单取件通知
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function orderPickupNotice(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/order/order-notice/pickup-notice', $param, []);
+    }
+
+    /**
+     * @description 订单超卖提醒
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function orderOversoldNotice(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/order/order-notice/oversold-notice', $param, []);
+    }
+}
--- src/Sdk/Refund/Refund.php
+++ src/Sdk/Refund/Refund.php
@@ -34,4 +34,103 @@ class Refund extends Sdk
     {
         return $this->request('POST', 'shop-api/service/refund/reject', $params, []);
     }
+
+    /**
+     * @description 售后单列表
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function list(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/list', $params, []);
+    }
+
+    /**
+     * @description 售后单详情
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function detail(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/detail', $params, []);
+    }
+
+    /**
+     * @description 售后退款留言
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function remark(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/remark', $params, []);
+    }
+
+    /**
+     * @description 售后订单导出
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function export(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/export', $params, []);
+    }
+
+    /**
+     * @description 修改退款物流信息
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function updateExpressDeliveryInfo(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/update-express-delivery-info', $params, []);
+    }
+
+    /**
+     * @description 售后商家确认收货（退货退款）
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function confirmReceiptGoodsOfRefunds(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/confirm-receipt-goods-of-refunds', $params, []);
+    }
+
+    /**
+     * @description 售后商家确认收货（退货换货）
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function confirmReceiptGoodsOfReturnAndExchange(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/confirm-receipt-goods-of-return-and-exchange', $params, []);
+    }
+
+    /**
+     * @description 售后商家寄回换货商品（退货换货）
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function sendGoodsOfReturnAndExchange(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/send-goods-of-return-and-exchange', $params, []);
+    }
+
+    /**
+     * @description 售后异常订单列表
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function refundExceptionOrderList(array $params): array
+    {
+        return $this->request('POST', 'shop-api/service/refund/refund-exception-order-list', $params, []);
+    }
 }
