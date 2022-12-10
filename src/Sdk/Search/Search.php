e4dce47c93a38e0bc768a1877e327635fc00b6f5
qqq <dev_line9@163.com>
2022-12-10 11:42:48 +0800

    feat: 新增广告、搜索的 sdk 接口

---
+++ src/Sdk/Search/Search.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Line9\Sdk\Sdk\Search;
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class Search extends Sdk
+{
+    protected function flag(): string
+    {
+        return 'shop';
+    }
+
+    /**
+     * @description 搜索管理数据列表
+     * @param array $params
+     * @param array $headers
+     * @param array $options
+     * @return array
+     * @throws SdkException
+     */
+    public function list(array $params, array $headers = [], array $options = []): array
+    {
+        return $this->request('post', 'shop-api/service/search/list', $params, $headers, $options);
+    }
+
+    /**
+     * @description 修改搜索数据
+     * @param array $params
+     * @param array $headers
+     * @param array $options
+     * @return array
+     * @throws SdkException
+     */
+    public function update(array $params, array $headers = [], array $options = []): array
+    {
+        return $this->request('post', 'shop-api/service/search/update', $params, $headers, $options);
+    }
+
+    /**
+     * @description 创建搜索数据
+     * @param array $params
+     * @param array $headers
+     * @param array $options
+     * @return array
+     * @throws SdkException
+     */
+    public function create(array $params, array $headers = [], array $options = []): array
+    {
+        return $this->request('post', 'shop-api/service/search/create', $params, $headers, $options);
+    }
+
+    /**
+     * @description 搜索数据详情
+     * @param array $params
+     * @param array $headers
+     * @param array $options
+     * @return array
+     * @throws SdkException
+     */
+    public function detail(array $params, array $headers = [], array $options = []): array
+    {
+        return $this->request('post', 'shop-api/service/search/detail', $params, $headers, $options);
+    }
+
+    /**
+     * @description 移除搜索数据
+     * @param array $params
+     * @param array $headers
+     * @param array $options
+     * @return array
+     * @throws SdkException
+     */
+    public function delete(array $params, array $headers = [], array $options = []): array
+    {
+        return $this->request('post', 'shop-api/service/search/delete', $params, $headers, $options);
+    }
+}
