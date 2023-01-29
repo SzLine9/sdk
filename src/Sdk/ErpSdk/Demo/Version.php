5eb6e088b3c44e37c56459c3bbca2cca23beffaa
王建民 <original@acodeway.com>
2023-01-29 11:57:45 +0800

    fix

---
+++ src/Sdk/ErpSdk/Demo/Version.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Line9\Sdk\Sdk\ErpSdk\Demo;
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class Version extends Sdk
+{
+    public function flag(): string
+    {
+        return 'erp';
+    }
+
+    /**
+     * @description 获取版本
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function getVersion(array $params): array
+    {
+        return $this->request('post', 'erp/api/version', $params, []);
+    }
+}
