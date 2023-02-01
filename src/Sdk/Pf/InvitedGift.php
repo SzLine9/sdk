1d24b3a66aa37eedfd7eaf5ebfdb28c0964ff845
王建民 <original@acodeway.com>
2023-02-01 16:39:54 +0800

    fix

---
+++ src/Sdk/Pf/InvitedGift.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Line9\Sdk\Sdk\Pf;
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class InvitedGift extends Sdk
+{
+    public function flag(): string
+    {
+        return 'shop';
+    }
+
+    /**
+     * @description 邀请礼包列表
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function list(array $params): array
+    {
+        return $this->request('post', 'shop-api/service/pf/invited-gift/list', $params, []);
+    }
+
+    /**
+     * @description 邀请礼包新增
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function add(array $params): array
+    {
+        return $this->request('post', 'shop-api/service/pf/invited-gift/add', $params, []);
+    }
+
+    /**
+     * @description 邀请礼包修改
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function update(array $params): array
+    {
+        return $this->request('post', 'shop-api/service/pf/invited-gift/update', $params, []);
+    }
+
+    /**
+     * @description 邀请礼包删除
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function delete(array $params): array
+    {
+        return $this->request('post', 'shop-api/service/pf/invited-gift/delete', $params, []);
+    }
+
+    /**
+     * @description 邀请礼包详情
+     * @param array $params
+     * @return array
+     * @throws SdkException
+     */
+    public function detail(array $params): array
+    {
+        return $this->request('post', 'shop-api/service/pf/invited-gift/detail', $params, []);
+    }
+}
