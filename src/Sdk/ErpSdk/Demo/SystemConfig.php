589b867c1c1405b93767f05198287811640ab458
o <o@o.local>
2023-04-18 00:18:30 +0800

    add erp system config

---
+++ src/Sdk/ErpSdk/Demo/SystemConfig.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Line9\Sdk\Sdk\ErpSdk\Demo;
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class SystemConfig extends Sdk
+{
+    protected function flag(): string
+    {
+        return 'erp';
+    }
+
+    /**
+     * @description get 配置
+     * @param array $params
+     * @param array $headers
+     * @param array $options
+     * @return array
+     * @throws SdkException
+     */
+    function getConfig(array $params, array $headers = [], array $options = []): array
+    {
+        return $this->request('post', '/erp-api/service/system/get_config', $params, $headers, $options);
+    }
+
+    /**
+     * @description set 配置
+     * @param array $params
+     * @param array $headers
+     * @param array $options
+     * @return array
+     * @throws SdkException
+     */
+    function setConfig(array $params, array $headers = [], array $options = []): array
+    {
+        return $this->request('post', '/erp-api/service/system/set_config', $params, $headers, $options);
+    }
+    
+}
