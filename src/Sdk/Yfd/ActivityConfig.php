d34d0a23712d5564f87b5a1ac9228124f57c7702
houyingcai <houyingcai@vchangyi.com>
2023-07-12 19:20:49 +0800

    新增云发单合集页

---
+++ src/Sdk/Yfd/ActivityConfig.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Line9\Sdk\Sdk\Yfd;
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class ActivityConfig extends Sdk
+{
+    protected function flag(): string
+    {
+        return 'shop';
+    }
+
+    /**
+     * @description 新增合集页
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function create(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/create', $param, []);
+    }
+
+    /**
+     * @description 编辑合集页
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function update(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/update', $param, []);
+    }
+
+    /**
+     * @description 合集页详情
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function detail(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/detail', $param, []);
+    }
+
+    /**
+     * @description 合集页详情
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function delete(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/delete', $param, []);
+    }
+
+    /**
+     * @description 合集页详情
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function list(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/list', $param, []);
+    }
+}
