0ec525e5aca30f759cc5d87352c918d35d64ad18
houyingcai <houyingcai@vchangyi.com>
2023-07-12 19:37:29 +0800

    添加open

---
+++ src/Sdk/Open/Agent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Line9\Sdk\Sdk\Open;
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class Agent extends Sdk
+{
+    protected function flag(): string
+    {
+        return 'open';
+    }
+
+    /**
+     * @description 代理创建
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function create(array $param): array
+    {
+        return $this->request('post', 'open/api/agent/create', $param, []);
+    }
+
+    /**
+     * @description 代理续费
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function renew(array $param): array
+    {
+        return $this->request('post', 'open/api/agent/renew', $param, []);
+    }
+
+    /**
+     * @description 代理详情
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function detail(array $param): array
+    {
+        return $this->request('post', 'open/api/agent/detail', $param, []);
+    }
+}
