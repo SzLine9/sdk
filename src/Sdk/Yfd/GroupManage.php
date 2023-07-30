f3e7a3218f19b63d1d01ccc9b94a31d6fca32fdb
houyingcai <houyingcai@vchangyi.com>
2023-07-30 20:48:31 +0800

    代码提交

---
+++ src/Sdk/Yfd/GroupManage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Line9\Sdk\Sdk\Yfd;
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class GroupManage extends Sdk
+{
+    protected function flag(): string
+    {
+        return 'shop';
+    }
+
+
+    /**
+     * @description 社群列表
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function detail(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/group_manage/list', $param, []);
+    }
+
+
+    /**
+     * @description 社群统计
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function groupCount(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/group_manage/count', $param, []);
+    }
+}
