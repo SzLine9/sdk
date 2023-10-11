ace46eb1142f01b6a1db83df7164c0219f6a2be8
503105827 <you@example.com>
2023-10-11 13:39:14 +0800

    新增分类合集页

---
+++ src/Sdk/Yfd/ActivityCategoryConfig.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace Line9\Sdk\Sdk\Yfd;
+
+
+use Line9\Sdk\Exception\SdkException;
+use Line9\Sdk\Sdk\Sdk;
+
+class ActivityCategoryConfig extends Sdk
+{
+    protected function flag(): string
+    {
+        return 'shop';
+    }
+
+    /**
+     * @description 新增合集页
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function create(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/category/create', $param, []);
+    }
+
+    /**
+     * @description 编辑合集页
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function update(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/category/update', $param, []);
+    }
+
+    /**
+     * @description 合集页详情
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function detail(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/category/detail', $param, []);
+    }
+
+    /**
+     * @description 合集页删除
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function delete(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/category/delete', $param, []);
+    }
+
+    /**
+     * @description 合集页列表
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function list(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/category/list', $param, []);
+    }
+
+    /**
+     * @description 合集页置顶/取消置顶
+     * @param array $param
+     * @return array
+     * @throws SdkException
+     */
+    public function changeTop(array $param): array
+    {
+        return $this->request('post', 'shop-api/service/yfd/category/change-top', $param, []);
+    }
+}
